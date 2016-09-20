15a78e8d360a856d45125ca0460e0c860fea61ba
boris.liu <boris_liu@foxmail.com>
2016-09-20 10:00:43 +0800

    add exception status for command
    Change-Id: If87104bfe1aa8c6a00497688eec5e5f7222da7af

--- src/com/utils/Constance.java
+++ src/com/utils/Constance.java
@@ -6,16 +6,17 @@ package com.utils;
  * 2016Äê9ÔÂ9ÈÕ
  */
 public class Constance {
-	public static final String STOP_TASK = "TASK:STOP taskId,threadId";
-	public static final String MAX_THREAD = "THRead:MAX:NUM threadNum"; 
-	public static final String RESTART_SERVER = "SERver:RESTART";
+	public static final String STOP_TASK       = "TASK:STOP taskId,threadId";
+	public static final String MAX_THREAD      = "THRead:MAX:NUM threadNum"; 
+	public static final String RESTART_SERVER  = "SERver:RESTART";
 	public static final String RESTART_PROCESS = "PROcess:RESTART";
-	public static final String START_PROCESS = "PROcess:START";
+	public static final String START_PROCESS   = "PROcess:START";
 	
 	public static class CommandStatus{
-		public static final int TODO = 1;
-		public static final int DOING = 2;
-		public static final int DONE = 3;
+		public static final int TODO      = 1;
+		public static final int DOING     = 2;
+		public static final int DONE      = 3;
+		public static final int EXCEPTION = 4;
 	}
 
 
